0cebd6c4bcfa7c8f62573e599c32cbf947e03b01
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-05-06 21:23:57 +0300

    HomeworkAlgorithms_003 updated

--- HomeworkAlgorithms_003/src/Main.java
+++ HomeworkAlgorithms_003/src/Main.java
@@ -5,16 +5,18 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         int[][] teams = {
-                { 45, 31, 24, 22, 20, 17, 14, 13, 12, 10 },
-                { 31, 18, 15, 12, 10, 8, 6, 4, 2, 1 },
-                { 51, 30, 10, 9, 8, 7, 6, 5, 2, 1 }
+                {45, 31, 24, 22, 20, 17, 14, 13, 12, 10},
+                {31, 18, 15, 12, 10, 8, 6, 4, 2, 1},
+                {51, 30, 10, 9, 8, 7, 6, 5, 2, 1}
         };
 
         int[] nationalTeam = mergeAll(teams);
         System.out.println(Arrays.toString(nationalTeam)); // [51, 45, 31, 31, 30, 24, 22, 20, 18, 17]
     }
 
-    /** Метод для слияния всех команд в одну национальную */
+    /**
+     * Метод для слияния всех команд в одну национальную
+     */
     public static int[] mergeAll(int[][] teams) {
         // Ваш код
         int[] teamFinal = teams[0];
@@ -27,13 +29,15 @@ public class Main {
         return teamFinal;
     }
 
-    /** Метод для слияния двух команд в одну */
+    /**
+     * Метод для слияния двух команд в одну
+     */
     public static int[] merge(int[] teamA, int[] teamB) {
         // Ваш код
         int counterTeamA = 0;
         int counterTeamB = 0;
         int counterTeamC = 0;
-        int[] teamC= new int[teamA.length];
+        int[] teamC = new int[teamA.length];
         while (counterTeamC < teamC.length) {
             if (teamA[counterTeamA] >= teamB[counterTeamB]) {
                 teamC[counterTeamC] = teamA[counterTeamA];
