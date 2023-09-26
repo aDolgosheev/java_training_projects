35bf9c22efa8433b6c5ed0d31c8a59d27158512f
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-09-26 23:23:46 +0300

    update 2023.09.26

--- spring-lessons/src/main/java/ru/dolgosheev/multithreading/task_018/Main.java
+++ spring-lessons/src/main/java/ru/dolgosheev/multithreading/task_018/Main.java
@@ -1,8 +1,36 @@
 package ru.dolgosheev.multithreading.task_018;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread.shutdown();
+    }
+}
+
+class MyThread extends Thread {
+    private volatile boolean running = true;
+
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
     }
 }
