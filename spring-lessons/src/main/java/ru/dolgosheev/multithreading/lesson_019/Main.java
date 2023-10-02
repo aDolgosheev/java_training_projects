cb05e83bf36ca5907b5932e28547a574c5b01777
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2023-10-02 22:56:16 +0300

    update 2023.10.02

---
+++ spring-lessons/src/main/java/ru/dolgosheev/multithreading/lesson_019/Main.java
@@ -0,0 +1,42 @@
+package ru.dolgosheev.multithreading.lesson_019;
+
+public class Main {
+    private int counter;
+
+    public static void main(String[] args) throws InterruptedException {
+        Main main = new Main();
+        main.doWork();
+    }
+
+    public void doWork() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    increment();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    increment();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(counter);
+    }
+
+    public synchronized void increment() {
+        counter++;
+    }
+}
